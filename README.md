ae588a8198a825acd918ccabd1312c3970436cf1
yangjh <1183762053@qq.com>
2021-05-25 17:59:07 +0800

    feat：初始化项目

---
+++ README.md
@@ -0,0 +1,4 @@
+# cloud_music
+歌词使用path_provider+File自定义Cache文件存储
+搜索历史使用的是SharedPreferences轻量级本地存储类
+
---
+++ lib/config/index.dart
@@ -0,0 +1,2 @@
+// const serviceHost = 'http://119.29.152.159:3030';
+const serviceHost = 'http://192.168.11.119:3030';
