4f5172544ba1b9afec13b98e715afd9879c4b109
yangjh <1183762053@qq.com>
2021-06-09 22:47:01 +0800

    fix:修复App返回不后台问题

--- android/app/src/main/kotlin/com/example/cloud_music/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/cloud_music/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.cloud_music
 
+import android.os.Bundle
+import android.os.Build
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs.BackgroundMode.transparent
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.*
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 
 class MainActivity: FlutterActivity() {
-  // private val CHANNEL = "android/back/desktop"
-  // override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-  //     registerWith(flutterEngine);
-  //     MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { methodCall, result ->
-  //         if (methodCall.method == "backDesktop") {
-  //             result.success(true)
-  //             moveTaskToBack(false)
-  //         }
-  //     }
-  // }
+  private val CHANNEL = "android/back/desktop"
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+      GeneratedPluginRegistrant.registerWith(flutterEngine);
+      MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { methodCall, result ->
+          if (methodCall.method == "backDesktop") {
+              result.success(true)
+              moveTaskToBack(false)
+          }
+      }
+  }
 }
